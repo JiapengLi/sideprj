094070e97f01f8049f593bfea9adb0d030e74587
JiapengLi <mail@jiapeng.me>
2020-10-22 12:37:26 +0800

    remove us support

--- src/app/template/main.c
+++ src/app/template/main.c
@@ -5,11 +5,10 @@
 void systick_evt(void);
 uint32_t millis(void);
 
-#define SYSTICK_1MS                         (1000000)
-#define SYSTICK_TIME_PER_TICK               (500)    // ns
+#define SYSTICK_TIME_PER_TICK               (500)           // ns
+#define SYSTICK_1MS                         (1000000)       // ns
 
 static volatile uint32_t systick_cnt = 0;
-static volatile uint32_t systick_cnt_us = 0;
 static volatile uint32_t systick_cnt_ms = 0;
 
 void systick_evt(void)
@@ -26,11 +25,6 @@ uint32_t millis(void)
     return systick_cnt_ms;
 }
 
-uint32_t micros(void)
-{
-    return systick_cnt;
-}
-
 //P1.07
 
 #define PIN         7
@@ -38,7 +32,6 @@ uint32_t micros(void)
 int main(void)
 {
     uint32_t ms = millis();
-    uint32_t us = micros();
     uint32_t pins_state;
 	
     /* enable 64MHz clock sourced by external 32MHz crystal */
